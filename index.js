ba92c0407aa93618af8719499eea144ab997a025
Replit user <>
2023-08-17 01:46:12 +0200

    Refactor + change route

--- index.js
+++ index.js
@@ -3,23 +3,21 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
-function req(request,response){
+function req(request, response) {
     const base = url.parse(request.url);
     const pathname = base.pathname;
     const fileName = '.' + pathname;
     const mainFile = path.join(__dirname, "./main.html")
-    const file = fs.readFileSync(mainFile, );
+    const file = fs.readFileSync(mainFile,);
     console.log(fileName);
-    fs.readFile(fileName,function(err,data){
-    if(err){
-        response.write(file);
+    fs.readFile(fileName, function (err, data) {
+        if (err) {
+            response.write(file);
+            return response.end();
+        }
+        response.write(data);
         return response.end();
-    }
-    response.write(data);
-    return response.end();
-    })  
+    })
 }
 http.createServer(req).listen(4097);
 console.log('Server started, http://localhost:4097');
-
-// about.html
