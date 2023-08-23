6d436583645201109937492fd67be9e9fd0b85b8
Replit user <>
2023-08-24 01:39:31 +0200

    finally

--- index.ts
+++ index.ts
@@ -1,12 +1,14 @@
 import express, { Request, Response } from 'express'
 import { join } from 'path'
-
 const app = express()
 const path = join(__dirname, '..', 'frontend')
+const main = join(__dirname, '..', 'main')
 
-        app.use('/', express.static(path))
+app.use('/', express.static(path))
 
-        app.all('*', (req: Request, res: Response) => res.sendStatus(404))
+app.use('/main', express.static(main))
 
-        app.listen(8000)
-        console.log("http://localhost:8000")
+app.all('*', (req: Request, res: Response) => res.sendStatus(404))
+
+app.listen(8000)
+console.log("http://localhost:8000")
