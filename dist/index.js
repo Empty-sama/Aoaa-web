0b18b544cbcda5cad5514d5cc606039b9b48558e
Replit user <>
2023-08-24 20:38:26 +0200

    webbb

--- dist/index.js
+++ dist/index.js
@@ -8,9 +8,8 @@ const path_1 = require("path");
 const app = (0, express_1.default)();
 const path = (0, path_1.join)(__dirname, '..', 'frontend');
 const about = (0, path_1.join)(__dirname, '..', 'about');
-app.use('/', express_1.default.static(path));
+app.use('/' || '/home', express_1.default.static(path));
 app.use('/about', express_1.default.static(about));
 app.all('*', (req, res) => res.sendStatus(404));
 app.listen(8000);
-console.log("http://localhost:8000");
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -4,11 +4,10 @@ const app = express()
 const path = join(__dirname, '..', 'frontend')
 const about = join(__dirname, '..', 'about')
 
-app.use('/', express.static(path))
+app.use('/' || '/home', express.static(path))
 
 app.use('/about', express.static(about))
 
 app.all('*', (req: Request, res: Response) => res.sendStatus(404))
 
 app.listen(8000)
-console.log("http://localhost:8000")
